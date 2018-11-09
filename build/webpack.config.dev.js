69708c70188aadba5605f64217b47339e2a89ea6
Chirath R <chirath.02@gmail.com>
2018-11-09 14:36:30 +0530

    Web pack configuration to watch and build

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,19 +1,40 @@
-var { VueLoaderPlugin } = require('vue-loader');
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugn = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: [
     './src/app.js'
   ],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
   module: {
     rules: [
       {
         test: /\.vue$/,
         use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
   plugins: [
-    new VueLoaderPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugn({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
   ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <h1>Hello World!</h1>
+  <div class="center-content">
+    <h1>Hello World !</h1>
   </div>
 </template>
