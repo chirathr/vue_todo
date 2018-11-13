a42fdbdbd1a1d587d3b245a75c338d56603b14ce
Chirath R <chirath.02@gmail.com>
2018-11-13 14:59:58 +0530

    Added a component for adding todo

---
+++ src/components/todoTemplate.js
@@ -0,0 +1,16 @@
+var TodoTemplate = {
+    template: `
+    <div class="col-md-6">
+        <div class="card mt-4" v-on:click="$emit('add-todo')">
+            
+            <div class="card-body row">   
+              
+                 <i class="material-icons add-button">add_circle</i>
+            
+            </div>
+        </div>
+    </div>
+    `
+}
+
+export default TodoTemplate;
